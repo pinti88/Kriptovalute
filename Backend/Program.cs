2dcdea402f7cfb0ac8607483d188780eea4f2ce0
pinti88 <tin.pinta@gmail.com>
2025-02-05 17:05:15 +0100

    json

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,18 +5,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// dodati ovu liniju za swagger
+builder.Services.AddSwaggerGen();
+
+
+// dodavanje kontaksta baze podataka - dependency injection
+builder.Services.AddDbContext<KriptovaluteContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext"));
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// dodati ove dvije linije za swagger
+app.UseSwagger();
+app.UseSwaggerUI(o => {
+    o.EnableTryItOutByDefault();
+    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+});
 
 app.MapControllers();
 
