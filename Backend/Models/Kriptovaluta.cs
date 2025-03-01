77d8a242d6e2735099d3064e5d99bf8b0e077b6f
pinti88 <tin.pinta@gmail.com>
2025-03-01 12:45:27 -0800

    izmjena models

--- Backend/Models/Kriptovaluta.cs
+++ Backend/Models/Kriptovaluta.cs
@@ -1,22 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Models
 {
-    public class Kriptovaluta 
+    public class Kriptovaluta
     {
         [Key]
-        public int Kripto_id { get; set; }
-        public string Ime { get; set; } = "";
-        public string Simbol { get; set; } = "";
-
+        public int Kripto_id { get; set; } 
+        [Required]
+        public string Ime { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(10)]
+        public string Simbol { get; set; } = string.Empty;
         public decimal? Cijena { get; set; }
-
         public decimal? Trzisna_vrjednost { get; set; }
-
         public decimal Volumen { get; set; }
-
-        public ICollection<Wallet>? Walleti { get; } = [];
-
+        public ICollection<Transakcija>? Transakcije { get; set; } = new List<Transakcija>(); 
+        public ICollection<Wallet>? Walleti { get; set; } = new List<Wallet>();
     }
 }
